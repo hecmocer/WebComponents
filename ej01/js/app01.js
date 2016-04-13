43525d119053b9d63c0a2631ed80d5d0b62a69f3
Héctor <hecmocer@gmail.com>
2016-04-13 11:59:15 +0200

    Ejercicio ficha de contacto con shadow DOM

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -4,8 +4,4 @@ var result = 0;
 // Inicialización del host, el shadow root y su contenido
 var host = document.querySelector('#host');
 var root = host.createShadowRoot();
-root.innerHTML = "<div id='shadowHost'>Este es el nodo host en la sombra.</div> <shadow></shadow>"
-
-var shadowHost = root.querySelector('#shadowHost');
-var shadowRoot = shadowHost.createShadowRoot();
-shadowRoot.innerHTML = "Este es el contenido del nodo anidado. <shadow></shadow>";
+root.innerHTML = document.querySelector('#HTMLimport').textContent;
