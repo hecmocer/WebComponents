10e7fbeddfcf562f3e24cb4d52eba114ee48c734
Héctor <hecmocer@gmail.com>
2016-04-13 10:58:21 +0200

    Ejercicio de anidamiento

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -3,9 +3,9 @@ var result = 0;
 
 // Inicialización del host, el shadow root y su contenido
 var host = document.querySelector('#host');
-var root1 = host.createShadowRoot();
-var root2 = host.createShadowRoot();
-var root3 = host.createShadowRoot();
-root1.innerHTML = "(1 Antes) <shadow></shadow> (1 Después)";
-root2.innerHTML = "(2 Antes) <shadow></shadow> (2 Después)";
-root3.innerHTML = "Contenido Capa 3: <shadow></shadow>";
+var root = host.createShadowRoot();
+root.innerHTML = "<div id='shadowHost'>Este es el nodo host en la sombra.</div> <shadow></shadow>"
+
+var shadowHost = root.querySelector('#shadowHost');
+var shadowRoot = shadowHost.createShadowRoot();
+shadowRoot.innerHTML = "Este es el contenido del nodo anidado. <shadow></shadow>";
