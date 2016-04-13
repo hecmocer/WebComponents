e03536a52b346739316bed3b1699899adc0cf66b
Héctor <hecmocer@gmail.com>
2016-04-13 13:56:10 +0200

    Ejercicio sobre plantillas y reutilización

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -1,11 +1,27 @@
 
-function createNew(container, header, body){
-    var root = document.querySelector(container).createShadowRoot();
-    var template = document.querySelector("#news").content;
-    template.querySelector(".header").textContent = header;
-    template.querySelector(".body").textContent = body;
+// Creamos prototipo
+var wcNewsPrototype = Object.create(HTMLElement.prototype);
+
+// Funcion de iniciliación del prototipo
+wcNewsPrototype.createdCallback = function(){
+    // This apunta al nodo hosts
+    var root = this.createShadowRoot();
+    var template = document.querySelector('#news').content;
+    // Toma los campos y los rellena en el template
+    template.querySelector('.header').textContent = this.querySelector('wc-news-header').textContent;
+    template.querySelector('.body').textContent = this.querySelector('wc-news-body').textContent;
+    template.querySelector('img').src = this.querySelector('wc-news-img').textContent;
     root.appendChild(template.cloneNode(true));
 }
 
-createNew("#news-01", "Este es el título", "Lorem Ipsum Dolor Sit Amet");
-createNew("#news-02", "La siguiente noticia es...", "la noticia es esta: bla bla bla");
+// Registra el elemento para añadir la etiqueta
+var News = document.registerElement(
+    'wc-news',
+    { prototype: wcNewsPrototype }
+);
+
+// Sobra ya que lo hace automaticamente en el HTML
+//var news01 = new News();
+//var news02 = new News();
+//document.body.appendChild(news01);
+//document.body.appendChild(news02);
