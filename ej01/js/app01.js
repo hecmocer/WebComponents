b960a18cf8025ed6b203dbe49df5cdd93e419ea2
Héctor <hecmocer@gmail.com>
2016-04-13 10:36:58 +0200

    Ejercicio con varias capas de shadow DOM

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -3,10 +3,9 @@ var result = 0;
 
 // Inicialización del host, el shadow root y su contenido
 var host = document.querySelector('#host');
-var root = host.createShadowRoot();
-root.innerHTML = document.querySelector('#innerHTMLjavascript').textContent;
-
-// Event listener del shadow dom
-root.querySelector('#button01').addEventListener('click', function(){
-    root.querySelector('#number01').innerHTML = result++;
-})
+var root1 = host.createShadowRoot();
+var root2 = host.createShadowRoot();
+var root3 = host.createShadowRoot();
+root1.innerHTML = "Contenido Capa 1";
+root2.innerHTML = "Contenido Capa 2";
+root3.innerHTML = "Contenido Capa 3";
