eb3ef647d465d04c4b39da6dbc1d717193245f9d
Héctor <hecmocer@gmail.com>
2016-04-14 09:26:41 +0200

    Ejercicio de templating con botones sin shadow dom

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -1,27 +1,20 @@
+var counter = 0;
 
 // Creamos prototipo
-var wcNewsPrototype = Object.create(HTMLElement.prototype);
+var wcBtnPrototype = Object.create(HTMLButtonElement.prototype);
 
 // Funcion de iniciliación del prototipo
-wcNewsPrototype.createdCallback = function(){
-    // This apunta al nodo hosts
-    var root = this.createShadowRoot();
-    var template = document.querySelector('#news').content;
-    // Toma los campos y los rellena en el template
-    template.querySelector('.header').textContent = this.querySelector('wc-news-header').textContent;
-    template.querySelector('.body').textContent = this.querySelector('wc-news-body').textContent;
-    template.querySelector('img').src = this.querySelector('wc-news-img').textContent;
-    root.appendChild(template.cloneNode(true));
+wcBtnPrototype.createdCallback = function(){
+    this.addEventListener('click', function(){
+        counter++;
+        document.querySelector('#counter').innerHTML = counter;
+    });
+
 }
 
 // Registra el elemento para añadir la etiqueta
-var News = document.registerElement(
-    'wc-news',
-    { prototype: wcNewsPrototype }
-);
-
-// Sobra ya que lo hace automaticamente en el HTML
-//var news01 = new News();
-//var news02 = new News();
-//document.body.appendChild(news01);
-//document.body.appendChild(news02);
+var wcBtn = document.registerElement(
+    'wc-button',
+    { extends: 'Button',
+    prototype: wcBtnPrototype }
+    );
