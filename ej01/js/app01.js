021ed172324fa8bd72a67b639d37a9121e31ca16
Héctor <hecmocer@gmail.com>
2016-04-14 09:54:52 +0200

    Ejercicio de import de HTML (aunque no funciona)

--- ej01/js/app01.js
+++ ej01/js/app01.js
@@ -1,20 +1,22 @@
-var counter = 0;
 
 // Creamos prototipo
-var wcBtnPrototype = Object.create(HTMLButtonElement.prototype);
+var wcNewsPrototype = Object.create(HTMLElement.prototype);
+var doc = document.currentScript.ownerDocument;
 
 // Funcion de iniciliación del prototipo
-wcBtnPrototype.createdCallback = function(){
-    this.addEventListener('click', function(){
-        counter++;
-        document.querySelector('#counter').innerHTML = counter;
-    });
-
+wcNewsPrototype.createdCallback = function(){
+    // This apunta al nodo hosts
+    var root = this.createShadowRoot();
+    var template = doc.querySelector('#news').content;
+    // Toma los campos y los rellena en el template
+    template.querySelector('.header').textContent = this.querySelector('wc-news-header').textContent;
+    template.querySelector('.body').textContent = this.querySelector('wc-news-body').textContent;
+    template.querySelector('img').src = this.querySelector('wc-news-img').textContent;
+    root.appendChild(template.cloneNode(true));
 }
 
 // Registra el elemento para añadir la etiqueta
-var wcBtn = document.registerElement(
-    'wc-button',
-    { extends: 'Button',
-    prototype: wcBtnPrototype }
-    );
+var News = document.registerElement(
+    'wc-news',
+    { prototype: wcNewsPrototype }
+);
