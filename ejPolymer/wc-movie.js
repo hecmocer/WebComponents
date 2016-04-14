a8e3c99b2c216250b4b7fe6bcff9161ca4e3a1a7
Héctor <hecmocer@gmail.com>
2016-04-14 11:04:26 +0200

    Ejercicio de definicion de propiedades en componente polymer

--- ejPolymer/wc-movie.js
+++ ejPolymer/wc-movie.js
@@ -2,11 +2,32 @@
 
     Polymer ({
         is: 'wc-movie',
-        ready: function(){
-            this.$.titulo.textContent = "Título de la película:";
-            this.$.portada.src = "http://placehold.it/350x150";
+        properties: {
+            title: {
+                type: String,
+                value: "Default"
+            },
+            imageSrc: {
+                type: String,
+                value: "http://placehold.it/350x150"
+            },
+            theater: {
+                type: String,
+                value: "default"
+            },
+            sesion: {
+                type: String,
+                value: "default"
+            },
+            code: {
+                type: String,
+                readonly: true,
+                computed: 'getCode(title, theater, sesion)'
+            },
+        },
+
+        getCode : function(title, theater, sesion){
+            return title + " " + theater + " " + sesion;
         }
-
     });
-
 })();
